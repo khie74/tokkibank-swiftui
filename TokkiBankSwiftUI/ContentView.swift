915b34d9e4a24efb0a678f50521b9120549758b6
KWANG SU SHIN <khie74@gmail.com>
2023-06-22 09:07:54 +0900

    Initial Commit

---
+++ TokkiBankSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TokkiBankSwiftUI
+//
+//  Created by KWANG SU SHIN on 2023/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
